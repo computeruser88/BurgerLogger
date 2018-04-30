d6519f55dc0832ca5cb1bf4a8776b05024f69936
Jan Balangue <jantonio.balangue@gmail.com>
2018-04-30 12:46:38 -0700

    Modifed server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8080;
 
 var app = express();
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.listen(PORT, function() {
     console.log("Burger Logger app listening on port " + PORT);
