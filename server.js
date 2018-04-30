1685736819406ab4c6635662b71c5d22f591aa1e
Jan Balangue <jantonio.balangue@gmail.com>
2018-04-30 12:43:53 -0700

    Modified server.js

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.listen(PORT, function() {
+    console.log("Burger Logger app listening on port " + PORT);
+})
